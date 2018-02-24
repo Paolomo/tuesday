d2ba6f658a947474367c8882a4bef2f9f0d244b8
Paolomo <Paolomo.rrissey@gmail.com>
2018-02-24 13:45:07 +0000

    initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+#	GIT	Usage	Example
+This	code	is	part	of	a	lecture	course	in	software	development
+##	Licence
+Author	:	Paulo
+Licensed	under	the	Creative	Commons	Attribution	3.0	License
+http://creativecommons.org/licenses/by/3.0/
+-	Free	for	use	in	both	personal	and	commercial	projects.
+-	Attribution	requires	leaving	author	name,	author	homepage	link,	and	the	license	info	intact.
---
+++ src/main/java/MorrisseyPaul/MyFirstMavenApp/App.java
@@ -0,0 +1,87 @@
+package MorrisseyPaul.MyFirstMavenApp;
+
+import java.util.Date;
+import java.util.Scanner;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	actionCommandlineInput(args);
+    	/*getSQLiteConnection();*/  
+    	getData();
+    }
+    
+	//define attributes
+   	private	Scanner someInput;
+   	private Date today;
+   	private static Logger LOG;
+   	private static String username="";
+   	
+  // constructor
+ 	public App()
+ 	{
+ 		LOG = LogManager.getLogger(App.class);
+ 		Configurator.setLevel(LOG.getName(), Level.DEBUG);
+ 		this.someInput = new Scanner(System.in);
+ 	    System.out.println(LOG.getName());
+ 		
+ 	    //do something here:
+ 		LOG.info( "Login check" );
+ 	     System.out.println("Please enter username:");
+         Scanner user = new Scanner(System.in);
+		
+         //user 
+         username = user.next();
+         System.out.println(username+" is best!!");
+         
+      	
+ 	}
+ 	
+    private static void actionCommandlineInput( String args[] )
+    {
+    	 App anApp = new App();
+    }
+    
+	private static void getData() {
+		// TODO Auto-generated method stub
+    	String url = "jdbc:sqlite:C://SQLiteStudio/Paololite.db";
+    	String sql="";
+    	String teacherId = "456";//hardcoded teacher id
+    	if(username.matches(teacherId)) {
+    	 sql  = "select pid, parent, SongId, SongTitle from users, music"
+    			+" where pid = Pid and SongTitle not null";// Null rows in musiC table
+    	}else {
+    	 sql  = "select pid, parent, SongId, SongTitle from users, music"
+    	    	+" where pid = Pid and SongTitle not null and pid="+username+"";
+    	}
+    
+    	try{
+    		
+    		Connection conn = DriverManager.getConnection(url);
+    		Statement stmt = conn.createStatement();
+    		ResultSet rs = stmt.executeQuery(sql);
+    	
+    			while(rs.next()) {
+    				System.out.println(rs.getInt("pid") + "\t" + rs.getString("parent") + "\t"+ rs.getInt("SongId") + "\t"+ rs.getString("Songtitle") +"\t");
+    		}
+    		
+    	}catch (SQLException e) {
+    		
+    		System.out.println(e.getMessage());
+    	}
+		
+	}
+}
+
